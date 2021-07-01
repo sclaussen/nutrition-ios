8a591bc7a09d2893497ee78e7f72a7fc1f5dff90
Shane Claussen <sclaussen@yahoo.com>
2021-07-01 10:00:54 -0700

    Initial Commit

---
+++ nutrition-ios/nutrition_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutrition_iosApp.swift
+//  nutrition-ios
+//
+//  Created by Shane Claussen on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct nutrition_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
